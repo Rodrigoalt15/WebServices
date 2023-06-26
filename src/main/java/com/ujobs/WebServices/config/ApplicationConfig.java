6150371013840396636a29188bc8363fc9b54c19
Rodrigo Lopez <64718167+Rodrigoalt15@users.noreply.github.com>
2023-06-26 09:47:23 -0500

    commit

--- src/main/java/com/ujobs/WebServices/config/ApplicationConfig.java
+++ src/main/java/com/ujobs/WebServices/config/ApplicationConfig.java
@@ -54,7 +54,7 @@ public class ApplicationConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:4200", "https://ujobsapp.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }
--- src/main/java/com/ujobs/WebServices/config/WebSocketConfig.java
+++ src/main/java/com/ujobs/WebServices/config/WebSocketConfig.java
@@ -21,7 +21,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:4200")
+                .setAllowedOrigins("http://localhost:4200", "https://ujobsapp.netlify.app")
                 .withSockJS();
     }
 
